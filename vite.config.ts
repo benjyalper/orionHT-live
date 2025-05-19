9f4c172b9a6fdc4a8ba94a6474494feb9f361567
benjyalper <benjyalper@hotmail.com>
2025-05-19 23:29:53 +0300

    docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/orionHT-live/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/'                 // on Railway & any other root‐served host
+    : '/orionHT-live/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
