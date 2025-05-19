44fcaab430a17505e3fa8401247c5a70902a486c
benjyalper <benjyalper@hotmail.com>
2025-05-19 22:00:52 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/OrionHT/',     // ← add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
